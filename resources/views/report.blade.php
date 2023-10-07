982b9de3818562bd1019ac0d1365b75f737c2ab1
Ziwana Rizwan Pramudia <ziwanarizwan@gmail.com>
2023-10-07 21:04:42 +0700

    update icon and title

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
+    <title>Laporan</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/icon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 </head>
