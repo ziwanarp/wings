43caf20a29f18d1386b0640b3b2953b030c9b055
Ziwana Rizwan Pramudia <ziwanarizwan@gmail.com>
2023-10-07 12:00:25 +0700

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,80 +25,60 @@
                             <!-- progressbar -->
                             <ul id="progressbar">
                                 <li class="active" id="product"><strong>Product</strong></li>
-                                <li id="personal"><strong>Personal</strong></li>
-                                <li id="payment"><strong>Payment</strong></li>
                                 <li id="confirm"><strong>Finish</strong></li>
                             </ul>
                             <!-- fieldsets -->
                             <fieldset>
                                 <div class="form-card">
-                                    <h2 class="fs-title">Account Information</h2>
-                                    <input type="email" name="email" placeholder="Email Id"/>
-                                    <input type="text" name="uname" placeholder="UserName"/>
-                                    <input type="password" name="pwd" placeholder="Password"/>
-                                    <input type="password" name="cpwd" placeholder="Confirm Password"/>
+
+                                    <section style="background-color: #eee;">
+                                        <div class="container py-5">
+                                          <div class="row justify-content-center mb-3">
+                                            <div class="col-md-12 col-xl-10">
+                                              <div class="card shadow-0 border rounded-3">
+                                                <div class="card-body">
+                                                  <div class="row">
+                                                    <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                                                      <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(4).webp"
+                                                          class="w-100" />
+                                                        <a href="#!">
+                                                          <div class="hover-overlay">
+                                                            <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
+                                                          </div>
+                                                        </a>
+                                                      </div>
+                                                    </div>
+                                                    <div class="col-md-6 col-lg-6 col-xl-6">
+                                                      <h5 class="text-black">Quant trident shirts</h5>
+                                                      <div class="d-flex flex-row">
+                                                          <span class="text-danger"><s>$20.99</s></span>
+                                                        </div>
+                                                        <div class="mt-1 mb-0 text-muted small">
+                                                            <h4 class="mb-1 me-1">$13.99</h4>
+                                                        </div>
+                                                        <div>
+                                                            <p>Subtotal : Rp. 10000</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
+                                                      <div class="d-flex flex-column mt-4">
+                                                        <p>Satuan: PCS</p>
+                                                        <input id="qty" min="0" name="quantity" value="1" type="number"class="form-control form-control-sm" onkeyup='check()' />
+                                                        <button class="btn btn-primary btn-sm" type="button">Details</button>
+                                                      </div>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>                             
+                                        </div>
+                                    </section>
+
+
                                 </div>
                                 <input type="button" name="next" class="next action-button" value="Next Step"/>
-                            </fieldset>
-                            <fieldset>
-                                <div class="form-card">
-                                    <h2 class="fs-title">Personal Information</h2>
-                                    <input type="text" name="fname" placeholder="First Name"/>
-                                    <input type="text" name="lname" placeholder="Last Name"/>
-                                    <input type="text" name="phno" placeholder="Contact No."/>
-                                    <input type="text" name="phno_2" placeholder="Alternate Contact No."/>
-                                </div>
-                                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                                <input type="button" name="next" class="next action-button" value="Next Step"/>
-                            </fieldset>
-                            <fieldset>
-                                <div class="form-card">
-                                    <h2 class="fs-title">Payment Information</h2>
-                                    <div class="radio-group">
-                                        <div class='radio' data-value="credit"><img src="https://i.imgur.com/XzOzVHZ.jpg" width="200px" height="100px"></div>
-                                        <div class='radio' data-value="paypal"><img src="https://i.imgur.com/jXjwZlj.jpg" width="200px" height="100px"></div>
-                                        <br>
-                                    </div>
-                                    <label class="pay">Card Holder Name*</label>
-                                    <input type="text" name="holdername" placeholder=""/>
-                                    <div class="row">
-                                        <div class="col-9">
-                                            <label class="pay">Card Number*</label>
-                                            <input type="text" name="cardno" placeholder=""/>
-                                        </div>
-                                        <div class="col-3">
-                                            <label class="pay">CVC*</label>
-                                            <input type="password" name="cvcpwd" placeholder="***"/>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-3">
-                                            <label class="pay">Expiry Date*</label>
-                                        </div>
-                                        <div class="col-9">
-                                            <select class="list-dt" id="month" name="expmonth">
-                                                <option selected>Month</option>
-                                                <option>January</option>
-                                                <option>February</option>
-                                                <option>March</option>
-                                                <option>April</option>
-                                                <option>May</option>
-                                                <option>June</option>
-                                                <option>July</option>
-                                                <option>August</option>
-                                                <option>September</option>
-                                                <option>October</option>
-                                                <option>November</option>
-                                                <option>December</option>
-                                            </select>
-                                            <select class="list-dt" id="year" name="expyear">
-                                                <option selected>Year</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                                <input type="button" name="make_payment" class="next action-button" value="Confirm"/>
                             </fieldset>
                             <fieldset>
                                 <div class="form-card">
@@ -124,6 +104,12 @@
         </div>
     </div>
 </div>
+<script>
+     var check = function() {
+            var isi = document.getElementById('qty').value
+            document.getElementById('qty1').value = isi;
+        }
+</script>
 <script src="{{asset('assets/form/form.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
